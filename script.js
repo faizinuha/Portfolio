a83fc1cdd3c66e358ed5a25c8df27742dbebc368
MarDev <rozakadm@gmail.com>
2024-05-31 13:12:04 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+   
+    open();
+}
+function goToContact() {
+    window.location.href = 'https://github.com/faizinuha';
+}
